4eadd9d9b983d8a648bd6bea25f56dab63cbfd76
MatthieuGenovese <gm102445@etu.unice.fr>
2017-04-10 16:25:19 +0200

    oublie de push ce fichier

--- app/src/main/java/com/ifi/m1/business_card/MainActivity.java
+++ app/src/main/java/com/ifi/m1/business_card/MainActivity.java
@@ -1,5 +1,8 @@
 package com.ifi.m1.business_card;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,13 +16,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import static android.R.attr.category;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -41,6 +45,9 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        ContentValues values = new ContentValues();
+        ContactsAdapter adapter = new ContactsAdapter(this);
+        adapter.ajouter();
     }
 
     @Override

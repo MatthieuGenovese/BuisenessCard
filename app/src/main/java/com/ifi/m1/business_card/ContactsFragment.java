8d809ac8e51a0f0ed878f7147172b9d3e9303811
MatthieuGenovese <gm102445@etu.unice.fr>
2017-04-11 08:58:25 +0200

    modification de la couleur de la liste view

--- app/src/main/java/com/ifi/m1/business_card/ContactsFragment.java
+++ app/src/main/java/com/ifi/m1/business_card/ContactsFragment.java
@@ -2,6 +2,7 @@ package com.ifi.m1.business_card;
 
 import android.app.Fragment;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -19,7 +20,21 @@ public class ContactsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         vContacts = inflater.inflate(R.layout.contacts_layout, container, false);
         ContactsAdapter db = new ContactsAdapter(this.getActivity().getApplicationContext());
-        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this.getActivity().getApplicationContext(),android.R.layout.simple_list_item_2, db.getAllDatas(), new String[]{db.COLUMN_NAME_NOM , db.COLUMN_NAME_PRENOM}, new int[]{android.R.id.text1, android.R.id.text2} );
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this.getActivity().getApplicationContext(),android.R.layout.simple_list_item_2, db.getAllDatas(), new String[]{db.COLUMN_NAME_NOM , db.COLUMN_NAME_PRENOM}, new int[]{android.R.id.text1, android.R.id.text2} ){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view =super.getView(position, convertView, parent);
+
+                TextView textView = (TextView) view.findViewById(android.R.id.text1);
+                TextView textView2 = (TextView) view.findViewById(android.R.id.text2);
+
+            /*YOUR CHOICE OF COLOR*/
+                textView.setTextColor(Color.BLACK);
+                textView2.setTextColor(Color.BLACK);
+
+                return view;
+            }
+        };
         ListView list = (ListView)vContacts.findViewById(R.id.listContacts);
 
         list.setAdapter(adapter);

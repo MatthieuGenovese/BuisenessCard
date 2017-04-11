eaa399e6d62e49d115fab1d2b336c246683f8f20
jeremy <jeremyfroment@yahoo.fr>
2017-04-11 15:11:31 +0200

    Ajout des switch pour les paramères (sons, vibrations, notifications)

--- app/src/main/java/com/ifi/m1/business_card/PreferencesFragment.java
+++ app/src/main/java/com/ifi/m1/business_card/PreferencesFragment.java
@@ -6,6 +6,8 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class PreferencesFragment extends Fragment {
     View vPreferences;
@@ -13,7 +15,35 @@ public class PreferencesFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        vPreferences = inflater.inflate(R.layout.preferences_layout, container, false);
+        vPreferences = inflater.inflate(R.layout.parameters_layout, container, false);
+
+        Switch switchNotif = (Switch) vPreferences.findViewById(R.id.switch_notifications);
+        switchNotif.setChecked(true);
+        switchNotif.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                System.out.println("Notifications activés ? " + isChecked);
+            }
+        });
+
+        Switch switchVibration = (Switch) vPreferences.findViewById(R.id.switch_vibration);
+        switchVibration.setChecked(true);
+        switchVibration.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                System.out.println("Vibration activé ? " + isChecked);
+            }
+        });
+
+        Switch switchSound = (Switch) vPreferences.findViewById(R.id.switch_sound);
+        switchSound.setChecked(true);
+        switchSound.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                System.out.println("Son activé ? " + isChecked);
+            }
+        });
+
         return vPreferences;
     }
 }

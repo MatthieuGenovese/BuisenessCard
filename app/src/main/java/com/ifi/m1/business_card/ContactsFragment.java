1d1e162e038fa77c9f7a334f64bb7b5ab3989398
MatthieuGenovese <gm102445@etu.unice.fr>
2017-05-08 14:39:16 +0200

    ajouts d'un fragment bugué qui pue la merde

--- app/src/main/java/com/ifi/m1/business_card/ContactsFragment.java
+++ app/src/main/java/com/ifi/m1/business_card/ContactsFragment.java
@@ -15,20 +15,26 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class ContactsFragment extends ListFragment {
     View vContacts;
+    ViewGroup container;
+    LayoutInflater inflater;
     private ArrayList<String> results = new ArrayList<>();
     Button contactsList, generateCard;
-    TextView nom,prenom,numero;
+    EditText nomAffichageContacts;
+    TextView nom,prenom,numero, emailAffichageContacts, adresseAffichageContacts, professionAffichageContacts, telAffichageContacts;
     String nomString, prenomString, numeroString;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        this.inflater = inflater;
+        this.container = container;
         vContacts = inflater.inflate(R.layout.contacts_layout, container, false);
         return vContacts;
     }
@@ -42,6 +48,12 @@ public class ContactsFragment extends ListFragment {
         nom = (TextView)this.getActivity().findViewById(R.id.textNom);
         prenom = (TextView)this.getActivity().findViewById(R.id.textPrenom);
         numero = (TextView)this.getActivity().findViewById(R.id.textPhoneNumber);
+        nomAffichageContacts = (EditText) getActivity().findViewById(R.id.editTextNom);
+        emailAffichageContacts = (TextView) this.getActivity().findViewById(R.id.editTextEmail);
+        //professionAffichageContacts = (TextView) this.getActivity().findViewById(R.id.ed);
+        adresseAffichageContacts = (TextView) this.getActivity().findViewById(R.id.editTextVille);
+        telAffichageContacts = (TextView) this.getActivity().findViewById(R.id.editTextPhone);
+
         contactsList.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent i = new Intent(Intent.ACTION_PICK,
@@ -82,6 +94,17 @@ public class ContactsFragment extends ListFragment {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 String line = (String) getListView().getItemAtPosition(i);
                 String[] decompose = line.split(" ");
+                getActivity().setContentView(R.layout.profil_layout);
+                Carte card = db.selectionner(decompose[0], decompose[1]);
+                System.out.println(card.getNom());
+                getActivity().getFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.profil, new OneContactFragment())
+                        .addToBackStack(null)
+                        .commit();
+                //adresseAffichageContacts.setText(card.getAdresse());
+                //emailAffichageContacts.setText(card.getEmail());
+                //telAffichageContacts.setText(card.getTel());
                 System.out.println(db.selectionner(decompose[0], decompose[1]));
             }
         });
